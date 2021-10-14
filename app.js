c274128e57d58a88656ce49cd510a6cfa783a339
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-13 17:06:39 -0700

    fixed my random number generater and successful cypress test

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const button = document.getElementById('generate');
-const number = document.getElementById('number');
-const theRandomNumber = document.getElementById('random-num');
+const number = document.getElementById('random-num');
 
-Math.floor(Math.random() * 20) + 1;
+let num = (Math.floor(Math.random() * 100)) + 1;
 
 button.addEventListener('click', () =>{
-    let userGuess = Number(number.value);
-    console.log(userGuess);
+    number.textContent = num;
+    num = (Math.floor(Math.random() * 100)) + 1;
     
 
 });
